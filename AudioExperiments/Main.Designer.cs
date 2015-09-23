f73d421202c1ef2bf82465024ebf70bf9cb555a3
simonhuang <simonhuang989@hotmail.com>
2015-09-22 21:21:21 -0400

    generate first note

--- AudioExperiments/Main.Designer.cs
+++ AudioExperiments/Main.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace AudioExperiments
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -33,22 +33,24 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 54);
+            this.button1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(35, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(217, 149);
+            this.button1.Size = new System.Drawing.Size(193, 45);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.play_audio);
             // 
-            // Form1
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(261, 77);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Main";
+            this.Text = "Audio Experiments";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
         }
---
+++ AudioExperiments/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AudioExperiments
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void play_audio(object sender, EventArgs e)
+        { 
+            string filePath = @"C:\Users\simon_000\Music\test2.wav";
+            WaveGenerator wave = new WaveGenerator(WaveExampleType.ExampleSineWave);
+            wave.Save(filePath);            
+
+            SoundPlayer player = new SoundPlayer(filePath);
+            player.Play();
+        }
+    }
+}
