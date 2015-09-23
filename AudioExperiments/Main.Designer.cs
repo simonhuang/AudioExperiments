54e0c1b4f6d60f15a6046d92319d63b8aa8b6426
simonhuang <simonhuang989@hotmail.com>
2015-09-22 21:25:36 -0400

    ui change

--- AudioExperiments/Main.Designer.cs
+++ AudioExperiments/Main.Designer.cs
@@ -28,25 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.button1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(35, 12);
+            this.button1.ForeColor = System.Drawing.Color.Navy;
+            this.button1.Location = new System.Drawing.Point(66, 46);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 45);
+            this.button1.Size = new System.Drawing.Size(74, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Play";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.play_audio);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(261, 77);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(859, 441);
             this.Controls.Add(this.button1);
             this.Name = "Main";
             this.Text = "Audio Experiments";
